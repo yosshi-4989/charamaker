bcad074f3e7757523911afd62e99437d99c688b5
sample <sample@icloud.com>
2018-04-28 18:57:39 +0900

    submitボタンの追加

--- src/pages/chara-maker/chara-maker.ts
+++ src/pages/chara-maker/chara-maker.ts
@@ -7,7 +7,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'chara-maker.html',
 })
 export class CharaMakerPage {
-  avilities: Array<{name: string, point: number}> = [
+  abilities: Array<{name: string, point: number}> = [
     {name: 'STR', point: null},
     {name: 'DEX', point: null},
     {name: 'INT', point: null},
@@ -17,20 +17,16 @@ export class CharaMakerPage {
     {name: 'SIZ', point: null},
     {name: '', point: null},
     {name: 'EDU', point: null}
-  //  {name: 'IDEA', point: null},
-  //  {name: 'LUCK', point: null},
-  //  {name: 'KNOW', point: null},
-  //  {name: 'DB', point: null},
-  //  {name: 'MAXSAN', point: null},
   ];
-
-  points: Array<number> = Array.from(Array(25), (k, v) => v+1);
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad CharaMakerPage');
+  submitAbility() {
+    sessionStorage.setItem('ability', JSON.stringify(this.abilities))
+  }
+  ionViewWillEnter() {
+    this.abilities = JSON.parse(sessionStorage.getItem('ability'))
   }
 
 }
