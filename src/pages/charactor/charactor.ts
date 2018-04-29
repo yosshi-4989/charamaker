11f7a8813af7dd984f48de389a33d1ada02456e0
sample <sample@icloud.com>
2018-04-29 23:56:45 +0900

    キャラクター詳細画面の作成

--- src/pages/charactor/charactor.ts
+++ src/pages/charactor/charactor.ts
@@ -1,15 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the CharactorPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
 @IonicPage({
-  segment: 'charactor/:name',
+  segment: 'charactor/:id',
   defaultHistory: ['CharaListPage']
 })
 @Component({
@@ -17,13 +10,57 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'charactor.html',
 })
 export class CharactorPage {
-  charactor: any = {};
+  charactor: {
+    'parsonalData': {
+      'name': string, 
+      'kana': string, 
+      'age': number,
+      'sex': string,
+      'job': string,
+      'growPlace': string,
+      'memo': string
+    },
+    'ability': {
+      'STR': number, 'DEX': number, 'INT': number, 'IDEA': number,
+      'CON': number, 'APP': number, 'POW': number, 'LUCK': number,
+      'SIZ': number, 'SAN': number, 'EDU': number, 'KNOW': number,
+      'MAXSAN': number, 'DB': string
+    },
+    'status': {
+      'HP': number, 'MP': number,
+      'SANpoint': number
+    },
+    'skills': {name: string, point: number}[]
+  } = {
+    'parsonalData': {
+      'name': null, 
+      'kana': null, 
+      'age': null,
+      'sex': null,
+      'job': null,
+      'growPlace': null,
+      'memo': null
+    },
+    'ability': {
+      'STR': null, 'DEX': null, 'INT': null, 'IDEA': null,
+      'CON': null, 'APP': null, 'POW': null, 'LUCK': null,
+      'SIZ': null, 'SAN': null, 'EDU': null, 'KNOW': null,
+      'MAXSAN': null, 'DB': null
+    },
+    'status': {
+      'HP': null, 'MP': null,
+      'SANpoint': null
+    },
+    'skills': []
+  }
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
-  ionViewWillEnter() {
-    this.charactor = this.navParams.get('chara');
+  ionViewDidLoad() {
+    var i = this.navParams.get('id');
+    this.charactor = JSON.parse(localStorage.getItem('charaList'))[i];
+    console.log(this.charactor);
+    console.log(this.charactor.parsonalData.name);
   }
-
 }
